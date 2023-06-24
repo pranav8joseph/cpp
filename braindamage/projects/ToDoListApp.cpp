40a88772181754cabf71c63f1fe9f80d90b8bf5b
Pranav Joseph <pranav8joseph@gmail.com>
2023-06-23 20:37:32 -0400

    to do list app startted

---
+++ braindamage/projects/ToDoListApp.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <vector>
+#include <string> 
+
+int main() {
+
+    // takes task input and puts it in vector
+    
+    std::vector<std::string> user_tasks = {}; 
+
+    
+
+}
