dfb4fcac5e9f66e33a18e6f13b47f3bff8dbd89b
Pranav Joseph <pranav8joseph@gmail.com>
2023-07-01 20:48:25 -0400

    started tic tac toe got basic idea how to do it

--- braindamage/projects/TicTacToe.cpp
+++ braindamage/projects/TicTacToe.cpp
@@ -10,9 +10,98 @@ namespace std {
 
 int main() {
     cout << "Lets play some tic tac toe\n"; 
-    
+    cout << "Here is the board\n";
+    cout << "-----------\n";
+    cout << " 1 | 2 | 3 \n";
+    cout << "-----------\n";
+    cout << " 4 | 5 | 6 \n";
+    cout << "-----------\n";
+    cout << " 7 | 8 | 9 \n";
+    cout << "-----------\n";
+    cout << "Player 1 is X and Player 2 is O\n";
+    cout << "Player 1 goes first\n";
+    cout << "Enter the number of the space you want to place your piece\n";
+    cout << "Player 1: ";
+    int player1; 
+    int player2; 
+    cin >> player1; 
+
 
 }
 
 
 } 
+
+    // if (player1 == 1) {
+    //     cout << "-----------\n";
+    //     cout << " X | 2 | 3 \n";
+    //     cout << "-----------\n";
+    //     cout << " 4 | 5 | 6 \n";
+    //     cout << "-----------\n";
+    //     cout << " 7 | 8 | 9 \n";
+    //     cout << "-----------\n";
+    // } else if (player1 == 2) {
+    //     cout << "-----------\n";
+    //     cout << " 1 | X | 3 \n";
+    //     cout << "-----------\n";
+    //     cout << " 4 | 5 | 6 \n";
+    //     cout << "-----------\n";
+    //     cout << " 7 | 8 | 9 \n";
+    //     cout << "-----------\n";
+    // } else if (player1 == 3) {
+    //     cout << "-----------\n";
+    //     cout << " 1 | 2 | X \n";
+    //     cout << "-----------\n";
+    //     cout << " 4 | 5 | 6 \n";
+    //     cout << "-----------\n";
+    //     cout << " 7 | 8 | 9 \n";
+    //     cout << "-----------\n";
+    // }else if (player1 == 4) {
+    //     cout << "-----------\n";
+    //     cout << " 1 | 2 | 3 \n";
+    //     cout << "-----------\n";
+    //     cout << " X | 5 | 6 \n";
+    //     cout << "-----------\n";
+    //     cout << " 7 | 8 | 9 \n";
+    //     cout << "-----------\n";
+    // } else if (player1 == 5) {
+    //     cout << "-----------\n";
+    //     cout << " 1 | 2 | 3 \n";
+    //     cout << "-----------\n";
+    //     cout << " 4 | X | 6 \n";
+    //     cout << "-----------\n";
+    //     cout << " 7 | 8 | 9 \n";
+    //     cout << "-----------\n";
+    // } else if (player1 == 6) {
+    //     cout << "-----------\n";
+    //     cout << " 1 | 2 | 3 \n";
+    //     cout << "-----------\n";
+    //     cout << " 4 | 5 | X \n";
+    //     cout << "-----------\n";
+    //     cout << " 7 | 8 | 9 \n";
+    //     cout << "-----------\n";
+    // }  else if (player1 == 7) {
+    //     cout << "-----------\n";
+    //     cout << " 1 | 2 | 3 \n";
+    //     cout << "-----------\n";
+    //     cout << " 4 | 5 | 6 \n";
+    //     cout << "-----------\n";
+    //     cout << " X | 8 | 9 \n";
+    //     cout << "-----------\n";
+    // } else if (player1 == 8) {
+    //     cout << "-----------\n";
+    //     cout << " 1 | 2 | 3 \n";
+    //     cout << "-----------\n";
+    //     cout << " 4 | 5 | 6 \n";
+    //     cout << "-----------\n";
+    //     cout << " 7 | X | 9 \n";
+    //     cout << "-----------\n";
+    // } else if (player1 == 9) {
+    //     cout << "-----------\n";
+    //     cout << " 1 | 2 | 3 \n";
+    //     cout << "-----------\n";
+    //     cout << " 4 | 5 | 6 \n";
+    //     cout << "-----------\n";
+    //     cout << " 7 | 8 | X \n";
+    //     cout << "-----------\n";
+    // }
