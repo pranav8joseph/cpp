bc58e082277815f0414ad30af1460495b0d1d828
Pranav Joseph <pranav8joseph@gmail.com>
2023-07-04 12:37:38 -0400

    might work

--- braindamage/projects/ComputerToe.cpp
+++ braindamage/projects/ComputerToe.cpp
@@ -28,6 +28,21 @@ void start_game() {
     cin >> comp; 
     if (comp == 'y' || comp == 'Y') {
         cout << "You will play against the computer\n";
+        cout << "what would you like to play as? (x/o)\n";
+        cin >> player;
+        if (player == 'x' || player == 'X') {
+            cout << "You are x\n";
+            cout << "The computer is o\n";
+            cout << "You will go first\n";
+            cout << "make sure you don't choose a spot that is already taken or you will loose your turn\n";
+            cout << "Enter the number of the space you want to place your piece\n";
+        } else if (player == 'o' || player == 'O') {
+            cout << "You are o\n";
+            cout << "The computer is x\n";
+            cout << "The computer will go first\n";
+            cout << "make sure you don't choose a spot that is already taken or you will loose your turn\n";
+            cout << "Enter the number of the space you want to place your piece\n";
+        }
     } else if (comp == 'n' || comp == 'N'){
     cout << "If not you will play against another player\n";
     cout << "Player 1 is x and Player 2 is o\n";
@@ -133,106 +148,164 @@ void check_for_winner_2players() {
 
 // play against computer stuff 
 
-void computerAsX() {
-    vector<vector<int>> winningCombos = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-    
-    vector<int> availableMoves;
-    for (int i = 0; i < board.size(); i++) {
-        if (board[i] != "x" && board[i] != "o") {
-            availableMoves.push_back(i);
-        }
-    }
+void ComputerAsX_offense() {
 
-    // Check if the computer can win
-    for (const auto& combo : winningCombos) {
-        int count = 0;
-        int emptyIndex = -1;
-        for (int index : combo) {
-            if (board[index] == "x") {
-                count++;
-            } else if (board[index] != "o") {
-                emptyIndex = index;
+    // first check for all possible winning spots and if there is one choose it, if not check if the other player is about to win and block them. if not choose a spot that will put you 1 move closer to winning. if not choose a random spot.
+
+    if (board[0] == "x" && board[1] == "x" && board[2] == "3") {
+        board[2] = "x"; 
+    } else if (board[0] == "x" && board[2] == "x" && board[1] == "2") {
+        board[1] = "x";
+    } else if (board[1] == "x" && board[2] == "x" && board[0] == "1") {
+        board[0] = "x";
+    } else if (board[3] == "x" && board[4] == "x" && board[5] == "6") {
+        board[5] = "x";
+    } else if (board[3] == "x" && board[5] == "x" && board[4] == "5") {
+        board[4] = "x";
+    } else if (board[4] == "x" && board[5] == "x" && board[3] == "4") {
+        board[3] = "x";
+    } else if (board[6] == "x" && board[7] == "x" && board[8] == "9") {
+        board[8] = "x";
+    } else if (board[6] == "x" && board[8] == "x" && board[7] == "8") {
+        board[7] = "x";
+    } else if (board[7] == "x" && board[8] == "x" && board[6] == "7") {
+        board[6] = "x";
+    } else if (board[0] == "x" && board[3] == "x" && board[6] == "7") {
+        board[6] = "x";
+    } else if (board[0] == "x" && board[6] == "x" && board[3] == "4") {
+        board[3] = "x";
+    } else if (board[3] == "x" && board[6] == "x" && board[0] == "1") {
+        board[0] = "x";
+    } else if (board[1] == "x" && board[4] == "x" && board[7] == "8") {
+        board[7] = "x";
+    } else if (board[1] == "x" && board[7] == "x" && board[4] == "5") {
+        board[4] = "x";
+    } else if (board[4] == "x" && board[7] == "x" && board[1] == "2") {
+        board[1] = "x";
+    } else if (board[2] == "x" && board[5] == "x" && board[8] == "9") {
+        board[8] = "x";
+    } else if (board[2] == "x" && board[8] == "x" && board[5] == "6") {
+        board[5] = "x";
+    } else if (board[5] == "x" && board[8] == "x" && board[2] == "3") {
+        board[2] = "x";
+    } else if (board[0] == "x" && board[4] == "x" && board[8] == "9") {
+        board[8] = "x";
+    } else if (board[0] == "x" && board[8] == "x" && board[4] == "5") {
+        board[4] = "x";
+    } else if (board[4] == "x" && board[8] == "x" && board[0] == "1") {
+        board[0] = "x";
+    } else if (board[2] == "x" && board[4] == "x" && board[6] == "7") {
+        board[6] = "x";
+    } else if (board[2] == "x" && board[6] == "x" && board[4] == "5") {
+        board[4] = "x";
+    } else if (board[4] == "x" && board[6] == "x" && board[2] == "3") {
+        board[2] = "x";
+    } else if (board[0] == "o" && board[1] == "o" && board[2] == "3") {
+        board[2] = "x";
+    } else if (board[0] == "o" && board[2] == "o" && board[1] == "2") {
+        board[1] = "x";
+    } else if (board[1] == "o" && board[2] == "o" && board[0] == "1") {
+        board[0] = "x";
+    } else if (board[3] == "o" && board[4] == "o" && board[5] == "6") {
+        board[5] = "x";
+    } else if (board[3] == "o" && board[5] == "o" && board[4] == "5") {
+        board[4] = "x";
+    } else if (board[4] == "o" && board[5] == "o" && board[3] == "4") {
+        board[3] = "x";
+    } else if (board[6] == "o" && board[7] == "o" && board[8] == "9") {
+        board[8] = "x";
+    } else if (board[6] == "o" && board[8] == "o" && board[7] == "8") {
+        board[7] = "x";
+    } else if (board[7] == "o" && board[8] == "o" && board[6] == "7") {
+        board[6] = "x";
+    } else if (board[0] == "o" && board[3] == "o" && board[6] == "7") {
+        board[6] = "x";
+    } else if (board[0] == "o" && board[6] == "o" && board[3] == "4") {
+        board[3] = "x";
+    } else if (board[3] == "o" && board[6] == "o" && board[0] == "1") {
+        board[0] = "x";
+    } else if (board[1] == "o" && board[4] == "o" && board[7] == "8") {
+        board[7] = "x";
+    } else if (board[1] == "o" && board[7] == "o" && board[4] == "5") {
+        board[4] = "x";
+    } else if (board[4] == "o" && board[7] == "o" && board[1] == "2") {
+        board[1] = "x";
+    } else if (board[2] == "o" && board[5] == "o" && board[8] == "9") {
+        board[8] = "x";
+    } else if (board[2] == "o" && board[8] == "o" && board[5] == "6") {
+        board[5] = "x";
+    } else if (board[5] == "o" && board[8] == "o" && board[2] == "3") {
+        board[2] = "x";
+    } else if (board[0] == "o" && board[4] == "o" && board[8] == "9") {
+        board[8] = "x";
+    } else if (board[0] == "o" && board[8] == "o" && board[4] == "5") {
+        board[4] = "x";
+    } else if (board[4] == "o" && board[8] == "o" && board[0] == "1") {
+        board[0] = "x";
+    } else if (board[2] == "o" && board[4] == "o" && board[6] == "7") {
+        board[6] = "x";
+    } else if (board[2] == "o" && board[6] == "o" && board[4] == "5") {
+        board[4] = "x";
+    } else if (board[4] == "o" && board[6] == "o" && board[2] == "3") {
+        board[2] = "x";
+    } else if (board[0] == "x" && board[1] == "2" && board[2] == "3") {
+        board[1] = "x";
+    } else if (board[0] == "1" && board[1] == "x" && board[2] == "3") {
+        board[0] = "x"; 
+    } else if (board[0] == "1" && board[1] == "2" && board[2] == "x") {
+        board[1] == "x";
+    } else if (board[3] == "x" && board[4] == "5" && board[5] == "6") {
+        board[4] = "x";
+    } else if (board[3] == "4" && board[4] == "x" && board[5] == "6") {
+        board[3] = "x";
+    } else if (board[3] == "4" && board[4] == "5" && board[5] == "x") {
+        board[4] = "x";
+    } else if (board[6] == "x" && board[7] == "8" && board[8] == "9") {
+        board[7] = "x";
+    } else if (board[6] == "7" && board[7] == "x" && board[8] == "9") {
+        board[6] = "x";
+    } else if (board[6] == "7" && board[7] == "8" && board[8] == "x") {
+        board[7] = "x";
+    } else if (board[0] == "x" && board[3] == "4" && board[6] == "7") {
+        board[3] = "x";
+    } else if (board[0] == "1" && board[3] == "x" && board[6] == "7") {
+        board[0] = "x";
+    } else if (board[0] == "1" && board[3] == "4" && board[6] == "x") {
+        board[3] = "x";
+    } else if (board[1] == "x" && board[4] == "5" && board[7] == "8") {
+        board[4] = "x";
+    } else if (board[1] == "2" && board[4] == "x" && board[7] == "8") {
+        board[1] = "x";
+    } else if (board[1] == "2" && board[4] == "5" && board[7] == "x") {
+        board[4] = "x";
+    } else if (board[2] == "x" && board[5] == "6" && board[8] == "9") {
+        board[5] = "x";
+    } else if (board[2] == "3" && board[5] == "x" && board[8] == "9") {
+        board[2] = "x";
+    } else if (board[2] == "3" && board[5] == "6" && board[8] == "x") {
+        board[5] = "x";
+    } else if (board[0] == "x" && board[4] == "5" && board[8] == "9") {
+        board[4] = "x";
+    } else if (board[0] == "1" && board[4] == "x" && board[8] == "9") {
+        board[0] = "x";
+    } else if (board[0] == "1" && board[4] == "5" && board[8] == "x") {
+        board[4] = "x";
+    } else if (board[2] == "x" && board[4] == "5" && board[6] == "7") {
+        board[4] = "x";
+    } else if (board[2] == "3" && board[4] == "x" && board[6] == "7") {
+        board[2] = "x";
+    } else if (board[2] == "3" && board[4] == "5" && board[6] == "x") {
+        board[4] = "x";
+    } else {
+        // choose random spot on the board if that spot is not taken 
+        for (int i = 0; i < 9; i++) {
+            if (board[i] != "x" && board[i] != "o") {
+                board[i] = "x";
+                break;
             }
         }
-        if (count == 2 && emptyIndex != -1) {
-            board[emptyIndex] = "x";
-            return;
-        }
     }
-
-    // Check if the player is about to win and block their move
-    for (const auto& combo : winningCombos) {
-        int count = 0;
-        int emptyIndex = -1;
-        for (int index : combo) {
-            if (board[index] == "o") {
-                count++;
-            } else if (board[index] != "x") {
-                emptyIndex = index;
-            }
-        }
-        if (count == 2 && emptyIndex != -1) {
-            board[emptyIndex] = "x";
-            return;
-        }
-    }
-
-    // If neither winning nor blocking move is possible, make a random move
-    int randomIndex = rand() % availableMoves.size();
-    int move = availableMoves[randomIndex];
-    board[move] = "x";
 }
-
-void computerAsO() {
-    vector<vector<int>> winningCombos = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-
-    vector<int> availableMoves;
-    for (int i = 0; i < board.size(); i++) {
-        if (board[i] != "x" && board[i] != "o") {
-            availableMoves.push_back(i);
-        }
-    }
-
-    // Check if the computer can win
-    for (const auto& combo : winningCombos) {
-        int count = 0;
-        int emptyIndex = -1;
-        for (int index : combo) {
-            if (board[index] == "o") {
-                count++;
-            } else if (board[index] != "x") {
-                emptyIndex = index;
-            }
-        }
-        if (count == 2 && emptyIndex != -1) {
-            board[emptyIndex] = "o";
-            return;
-        }
-    }
-
-    // Check if the player is about to win and block their move
-    for (const auto& combo : winningCombos) {
-        int count = 0;
-        int emptyIndex = -1;
-        for (int index : combo) {
-            if (board[index] == "x") {
-                count++;
-            } else if (board[index] != "o") {
-                emptyIndex = index;
-            }
-        }
-        if (count == 2 && emptyIndex != -1) {
-            board[emptyIndex] = "o";
-            return;
-        }
-    }
-
-    // If neither winning nor blocking move is possible, make a random move
-    int randomIndex = rand() % availableMoves.size();
-    int move = availableMoves[randomIndex];
-    board[move] = "o";
-}
-
 
 void check_for_winner_computer() {
     if (board[0] == "x" && board[1] == "x" && board[2] == "x") {
@@ -295,37 +368,8 @@ int main() {
 // start game
     std::start_game();
     
-    if (comp == 'y' || comp == 'Y') {
-        std::cout << "which player would you like to be? (x or o)" << std::endl; 
-        std::cin >> player;
-        if (player == 'x' || player == 'X') {
-            std::cout << "You are player 1" << std::endl;
-            std::cout << "Computer is player 2" << std::endl;
-
-        for (int i = 0; i < 4; i++) {
-            std::player1_update_board(); 
-            std::check_for_winner_computer();
-            std::computerAsO(); 
-            std::check_for_winner_computer();
-            }
-
-        // 5th turn for x 
-        std::player1_update_board();
-        std::check_for_winner_computer();
-
-        } else if (player == 'o' || player == 'O') {
-            std::cout << "You are player 2" << std::endl;
-            std::cout << "Computer is player 1" << std::endl;
-        
-        for (int i = 0; i < 4; i++) { 
-            std::computerAsX(); 
-            std::check_for_winner_computer();
-            std::player2_update_board(); 
-            std::check_for_winner_computer();
-            }
-        }
-
-    } else if (comp == 'n' || comp == 'N') {
+    
+    if (comp == 'n' || comp == 'N') {
     for (int i = 0; i < 4; i++) {
     std::player1_update_board(); 
     std::check_for_winner_2players(); 
