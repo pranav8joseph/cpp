02b02195918f25425408c304df854994a190d340
pranav8joseph <pranav8joseph@gmail.com>
2023-07-26 22:06:00 -0400

    hi

--- braindamage/learning/main.cpp
+++ braindamage/learning/main.cpp
@@ -1,43 +1,11 @@
-#include <iostream>
-#include <string> 
+#include <iostream> 
+#include <string>
+#include <vector>
 
-using namespace std;
+using namespace std; 
 
 int main() {
 
-    int H; 
 
-    string W; 
-    cin >> W; 
     
-    int R; 
-    cin >> R; 
-
-    int C;
-    cin >> C; 
-
-    if (1 > C || C > 100) {
-        return 0; 
-        }
-    if (1 > R || R > 100) {
-        return 0; 
-    }
-
-    cin.ignore(); 
-
-string board;
-
-for (int i = 0; i < R; i++) {
-    string board;
-    getline(cin, board);
-}
-    // Iterate through the board
-    while (board.find(W) != string::npos) {
-        board.find(W); 
-        H++;
-        }
-
-cout << H << endl;
-    return 0;
-
 }
