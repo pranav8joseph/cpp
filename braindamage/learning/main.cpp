b684662d594135caf150034d7d19bbcf52ef96d1
Pranav <pranav8joseph@gmail.com>
2023-10-15 22:33:50 -0400

    commit

--- braindamage/learning/main.cpp
+++ braindamage/learning/main.cpp
@@ -1,11 +1,18 @@
 #include <iostream> 
 #include <vector>
 #include <string>
+#include <chrono> 
+#include <thread>
 
 using namespace std;
 
 int main() {
 
-    
+    for (int i = 0; i < 10000; i++) {
 
+        // 500 millisecond pause
+        cout << "hi\n"; 
+        this_thread::sleep_for(chrono::milliseconds(500));
+
+}
 }
