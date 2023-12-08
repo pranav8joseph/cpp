7e99f619e604c83a693ae9c09c7f6edc073c68a6
Pranav <pranav8joseph@gmail.com>
2023-12-08 10:47:30 -0500

    commit

--- braindamage/competitionproblems/ccc23j5.cpp
+++ braindamage/competitionproblems/ccc23j5.cpp
@@ -1,29 +1,7 @@
 #include <iostream> 
 #include <string> 
-#include <cstring> 
 
 using namespace std;
-
-// int* insertX(int n, int arr[], 
-//             int x, int pos) 
-// { 
-//     int l; 
- 
-//     // increase the size by 1 
-//     n++; 
- 
-//     // shift elements forward 
-//     for (i = n; i >= pos; i--) 
-//         arr[i] = arr[i - 1]; 
- 
-//     // insert x at pos 
-//     arr[pos - 1] = x; 
- 
-//     return arr; 
-// } 
-int n; 
-int d;
-int pos;
 
 int main() {
 
@@ -43,16 +21,13 @@ int main() {
     int C; 
     cin >> C;
     
-    char board[R][C*2];
-    char board2[R][C];
+    char board[R][C];
     // Storing user input in the array
-    for (int i = 0; i < R; i++) {
+     for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
             cin >> board[i][j];
-            // Add a space between characters
-            board[i][j+1] = ' ';
-            }
         }
+    }
 
 
     // print board
@@ -62,10 +37,16 @@ int main() {
     //     }
     // }
 
-    for (int i = 0; i < R; i++) {
-        for (int j = 0; j < C * 2; j++) {
-            cout << board[i][j];
+     for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            cout << board[i][j] << ' ';
         }
         cout << endl; 
     }
 }
+
+// F T R U B L K
+// P M N A X C U
+// A E R C N E O
+// M N E U A R M
+// M U N E M N S
