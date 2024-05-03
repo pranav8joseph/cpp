dd8a96461d74d227e063777515190dcd6d34f84f
pranav8joseph <pranav8joseph@gmail.com>
2024-05-02 23:28:30 -0400

    IM DOING SOMETHING RIGHT IDK WHAT IT IS BUT IM DOING IT

--- braindamage/competitionproblems/ccc23Junior/ccc23j5.cpp
+++ braindamage/competitionproblems/ccc23Junior/ccc23j5.cpp
@@ -3,27 +3,29 @@
 
 using namespace std;
 
-int main() {
+string W;
+int R;
+int C; 
+int H;
 
-    string W; 
+int main() {
+ 
     cin >> W; 
 
     // check if word is longer than 2 letters if not return 0
-    if (size(W) < 2) {
+    if (W.length() < 2) {
         return 0; 
     }
 
     // take rows number
-    int R;
     cin >> R;
 
     // take columns number
-    int C; 
     cin >> C;
     
-    char board[R][C];
+    string board[R][C];
     // Storing user input in the array
-     for (int i = 0; i < R; i++) {
+    for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
             cin >> board[i][j];
         }
@@ -37,12 +39,68 @@ int main() {
     //     }
     // }
 
-     for (int i = 0; i < R; i++) {
+    //iterate to find word
+    // check rows
+    int l = 0; 
+    int l2 = 0; 
+    for (int i = 0; i < R; i++) {
+        for (int j = 0; j < C; j++) {
+            // check going right
+            for (int d = 0; d < W.length(); d++) {
+                if (board[i][j+d] == W.at(d)) {
+                    l += 1; 
+                }
+            } //check going left
+            if (board[i][j] == W.at(W.length()-1)) {
+                for(int t = 0; t < W.length(); t++) {
+                    if (board[i][j+t] == W.at(W.length()-1-t)) {
+                        l2 += 1; 
+                    }
+                }
+            }
+        }
+    }
+
+    if (l == W.length()) {
+        H += (l/W.length()); 
+    }
+
+    if (l2 == W.length()) {
+        H += (l2/W.length()); 
+    }
+
+
+// FINISH NEXT """""""""""" (same technique as rows checker)
+    int q = 0; 
+    int q2 = 0; 
+// check columns 
+    for (int j = 0; j < C; j++) {
+        for (int i = 0; i < R; i++) {
+            // check up
+            if (board[i][j] == "M" && board[i+1][j] == "E" && board[i+2][j] == "N" && board[i+3][j] == "U") {
+                H += 1; 
+            // check down
+            } else if (board[i][j] == "U" && board[i+1][j] == "N" && board[i+2][j] == "E" && board[i+3][j] == "M") {
+                H += 1; 
+            }
+        }
+    }
+
+    // RIGHT ANGLE SWITCHES DO THIS AFTER COLUMN CHECKER """"""
+    // for (int i = 0; i < C; i++) {
+    //     for (int j = 0; j < R; j++) {
+    //         if (board)
+    //     }
+    // }
+
+    for (int i = 0; i < R; i++) {
         for (int j = 0; j < C; j++) {
             cout << board[i][j] << ' ';
         }
-        cout << endl; 
+    cout << endl; 
     }
+
+    cout << H << endl; 
 }
 
 // F T R U B L K
